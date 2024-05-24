b0109f266c258287c644f1761962e07a2839842a
caomingming <cinderella_cmm@sina.cn>
2024-05-25 00:07:41 +0800

    first commit

---
+++ src/components/CommonAside.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-aside width="200px">
+    <el-menu class="el-menu-vertical-demo">
+      <el-menu-item index="2">
+        <el-icon><icon-menu /></el-icon>
+        <span>Navigator Two</span>
+      </el-menu-item>
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon><location /></el-icon>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item two</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group Two">
+          <el-menu-item index="1-3">item three</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="1-4">
+          <template #title>item four</template>
+          <el-menu-item index="1-4-1">item one</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <CommonAside />
+      <el-container>
+        <CommonHeader />
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+import CommonHeader from "../components/CommonHeader.vue"
+import CommonAside from "../components/CommonAside.vue"
+</script>
+
+<style lang="scss" scoped></style>
